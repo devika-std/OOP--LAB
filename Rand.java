7204962cd948f0ad4fafc0134f7ceb341d908a32
devika-std <devikasuresh26@gmail.com>
2025-02-21 11:18:56 +0530

    Add files via upload

---
+++ Rand.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+public class Rand 
+{
+    public static void main(String args[]){
+    System.out.println("First number:"+Math.random());
+    System.out.println("second number:"+Math.random());
+    }
+
+    
+}
