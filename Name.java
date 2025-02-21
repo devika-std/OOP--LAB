088b0f2e357fcb78ad7d58e868d6cbe5794066a6
devika-std <devikasuresh26@gmail.com>
2025-02-21 11:04:17 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,13 @@
+public class Name 
+{
+    public static void main(String args[])
+    {
+        int age=10;
+        String name="Devika";
+        System.out.println("Name: " +name+ "Age: " +age );
+
+
+    }
+
+    
+}
