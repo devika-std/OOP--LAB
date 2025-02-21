02e4a1e033ba4b86746c28ab68c599dd61ad2990
devika-std <devikasuresh26@gmail.com>
2025-02-21 11:05:29 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,11 @@
+public class Course
+ {
+    public static void main(String args[])
+    {
+        int no=22;
+        String course1="MCA";
+        System.out.println("Course :"+course1);
+        System.out.println("Roll No. :"+no);
+    }
+    
+}
