a75beeaad0ea2e2937f738d099947e4ce720bc60
devika-std <devikasuresh26@gmail.com>
2025-02-21 11:20:58 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,14 @@
+public class Emp 
+{
+    void displayMsg()
+    {
+        System.out.println("Employee Details");
+    }
+    public static void main(String[] args) 
+    {
+        Emp e=new Emp();
+        e.displayMsg();
+        
+    }
+    
+}
